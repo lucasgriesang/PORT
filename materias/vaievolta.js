a927cf7fcdba3908b8768a5dcaf5c3de0c373100
lucas_gd_oliveira <lucas_gd_oliveira@estudante.sesisenai.org.br>
2024-03-26 11:36:54 -0300

    Portfolío

---
+++ materias/vaievolta.js
@@ -0,0 +1,21 @@
+function humanas(){
+    window.location.href = "http://127.0.0.1:5500/materias/humanas.html"
+}
+function linguagem(){
+    window.location.href = "http://127.0.0.1:5500/materias/linguagem.html"
+}
+function matematica(){
+    window.location.href = "http://127.0.0.1:5500/materias/matematica.html"
+}
+function Natureza(){
+    window.location.href = "http://127.0.0.1:5500/materias/natureza.html"
+}
+function tecnico(){
+    window.location.href = "http://127.0.0.1:5500/materias/tecnico.html"
+}
+function voltar(){
+    window.location.href = "http://127.0.0.1:5500/pagina%20inicial/main.html"
+}
+function volta2(){
+    window.location.href = "http://127.0.0.1:5500/materias/capatodasaulas.html"
+}
---
+++ pagina inicial/main.js
@@ -0,0 +1,19 @@
+function capa() {
+    window.location.href = "http://127.0.0.1:5500/materias/capatodasaulas.html"
+}
+function link() {
+    window.location.href = "https://www.linkedin.com/in/lucas-griesang-dieckmann-de-oliveira-5b7202249/"
+}
+function GIT() {
+    window.location.href = "https://github.com/lucasgriesang"
+}
+function ports() {
+    window.location.href = "http://127.0.0.1:5500/ajudas/contato.html"
+}
+function ppe() {
+    window.location.href = "http://127.0.0.1:5500/materias/PPE.html"
+}
+function certificados() {
+    window.location.href = "http://127.0.0.1:5500/materias/PPE.html"
+
+}
