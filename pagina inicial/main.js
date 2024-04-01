e4f04af991d5919cb4d5535572c26f3c4f4c9b7c
lucasgriesang <127210973+lucasgriesang@users.noreply.github.com>
2024-04-01 07:40:17 -0300

    Update main.js

--- pagina inicial/main.js
+++ pagina inicial/main.js
@@ -5,13 +5,10 @@ function link() {
     window.location.href = "https://www.linkedin.com/in/lucas-griesang-dieckmann-de-oliveira-5b7202249/"
 }
 function GIT() {
-    window.location.href = "https://github.com/lucasgriesang"
-}
-function ports() {
-    window.location.href = "https://lucasgriesang.github.io/PORT"
+    window.open = "https://github.com/lucasgriesang"
 }
 function ppe() {
-    window.location.href = "https://lucasgriesang.github.io/PORT/materias/PPE.html"
+    window.open = "https://lucasgriesang.github.io/PORT/materias/PPE.html"
 }
 function certificados() {
     window.location.href = "hhttps://lucasgriesang.github.io/PORT/materias/PPE.html"
